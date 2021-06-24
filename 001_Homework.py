06fc78225a68618200ac72ca6f80c53c72ae1b0b
JanaSed <j.sedasheva@gmail.com>
2021-06-24 18:13:49 +0300

    Homework #1

--- 001_Homework.py
+++ 001_Homework.py
@@ -4,10 +4,8 @@ if (int(number) % 2) == 0:
     print('The number', (number), 'is even')
 else:
     print('The number', (number), 'is odd')
-
 # возведение в квадрат
 print('Your number', (number), 'raised to the 2nd power is:', float(number) ** 2)
-
 # количество символов в числе
 if len(str(number)) == 1:
     print('There is', len(str(number)), 'digit in your number')
