4379f308b8d2e782790eb9ae3bfce1e1bd9c5775
JanaSed <j.sedasheva@gmail.com>
2021-07-01 23:45:01 +0300

    Homework #3

--- 003_ID_check/ID_input.py
+++ 003_ID_check/ID_input.py
@@ -1,14 +1,19 @@
-try:
-    user_code = input('Please enter your ID: ')
-    if len(user_code) != 11:
-        raise UserWarning
-    user_code = int(user_code)
-except UserWarning:
-    print('Entered ID is incorrect, doesn\'t consist 11 numbers.')
-except ValueError:
-    print('Entered ID is not numeric')
-else:
-    print(input('Please choose an option:\n'
+while True:
+    try:
+        user_code = input('Please enter your ID: ')
+        user_code = str(int(user_code))
+        print(user_code)
+        if len(user_code) != 11:
+            raise UserWarning
+
+    except UserWarning:
+        print('Entered ID is incorrect, doesn\'t consist 11 numbers.')
+        continue
+    except ValueError:
+        print('Entered ID is not numeric')
+        continue
+    else:
+        print(input('Please choose an option:\n'
                               '1. ID data\n'
                               '2. Validate\n'
                               '3. Exit\n'
