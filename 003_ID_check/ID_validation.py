0cb2cceb817a27a127abbf42c849089b093c68ea
JanaSed <j.sedasheva@gmail.com>
2021-07-02 14:07:58 +0300

    Homework #3

--- 003_ID_check/ID_validation.py
+++ 003_ID_check/ID_validation.py
@@ -4,13 +4,13 @@ check_2 = [3,4,5,6,7,8,9,1,2,3]
 
 user_code = input('Please enter your ID: ')
 # print(type(user_code))
-
+x = 0
 for check in range(0, 10):
-    x = check_1[check] * int(user_code[check])
+    x += check_1[check] * int(user_code[check])
 y = x % 11
 if y >= 10:
     for check in range(0, 10):
-        x = check_2[check] * int(user_code[check])
+        x += check_2[check] * int(user_code[check])
     y = x % 11
 if y == int(user_code[10]):
     print('ID is valid')
