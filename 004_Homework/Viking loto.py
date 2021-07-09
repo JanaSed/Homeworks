172bf25792b847813123dca14b1acc36300902ae
JanaSed <j.sedasheva@gmail.com>
2021-07-09 12:52:07 +0300

    update

--- 004_Homework/Viking loto.py
+++ 004_Homework/Viking loto.py
@@ -1,9 +1,9 @@
 from random import randint
 
-for x in range(10):
-    a = randint(1, 100)
+for x in range(5):
+    a = randint(1, 48)
     print(a, end=' ')
 print()
 for y in range(2):
-    b = randint(1, 10)
+    b = randint(1, 5)
     print(b, end=' ')
