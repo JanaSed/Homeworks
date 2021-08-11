13d465d6816445015ebe77210827696f6ec8f649
JanaSed <j.sedasheva@gmail.com>
2021-08-11 11:56:19 +0300

    update

--- 004_Homework/euro_jackpot.py
+++ 004_Homework/euro_jackpot.py
@@ -1,3 +1,4 @@
+import random
 from random import randint
 
 for x in range(5):
@@ -7,3 +8,8 @@ print()
 for y in range(2):
     b = randint(1, 5)
     print(b, end=' ')
+
+print()
+
+print(random.sample(range(50), 5))
+print(random.sample(range(1, 5), 2))
---
+++ 004_Homework/viking_loto.py
@@ -0,0 +1,5 @@
+import random
+from random import randint
+
+print(random.sample(range(48), 6))
+print(random.sample(range(1, 5), 1))
